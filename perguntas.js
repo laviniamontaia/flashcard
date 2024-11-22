204d5ef4d3e161877e1a1934ae4e94bebdae8012
laviniamontaia <00001092439742sp@al.educacao.sp.gov.br>
2024-11-22 09:43:07 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'SÉRIE',
+    'Qual é o nome do personagem que se torna o interesse romântico de Daphne na primeira temporada?',
+    'Simon Basset, o Duque de Hastings'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'SÉRIE',
+    'Qual série é baseada em um livro de George R.R. Martin e é conhecida por suas batalhas épicas e traições?',
+    'Game of Thrones'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'FILME',
+    'Qual é o nome do ator que interpreta o personagem "Iron Man" nos filmes da Marvel?',
+    'Robert Downey Jr.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'FILME',
+    'Quem é o diretor do filme "Titanic"?',
+    'James Cameron'
 )
